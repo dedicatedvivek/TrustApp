5eb915795a915bc8a99867273c0b18f8ef9cc0a6
kindkarthik <vivekiyer98@gmail.com>
2018-07-22 20:16:09 +0530

    kar

---
+++ resources/views/transaction.blade.php
@@ -0,0 +1,28 @@
+<html>
+
+<center>
+<h2 style="color: fefe70">TRANSACTIONS</h2>
+
+
+<style>
+
+body{
+	background-image: url("formback.png");
+}
+
+
+</style>
+
+<body>
+<br> <br><br>
+<button name="FIXED DEPOSIT">FIXED DEPOSIT</button>
+&nbsp &nbsp &nbsp &nbsp
+<button name="SAVINGS ACCOUNT">SAVINGS ACCOUNT</button>	
+&nbsp &nbsp &nbsp &nbsp
+<button name="MISCELLANEOUS">MISCELLANEOUS</button>
+<br><br><br><br><br><br>
+<button name="BACK" onclick="window.location='/'" style="width: 10%">BACK</button>
+
+</center>
+</body>
+</html>

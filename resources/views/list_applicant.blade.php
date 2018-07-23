0c76037213f2b1e9c3998cb1489576c2b3297d41
dedicatedvivek <vivekiyer98@gmail.com>
2018-07-23 09:31:08 +0530

    viv

--- resources/views/list_applicant.blade.php
+++ resources/views/list_applicant.blade.php
@@ -2,11 +2,11 @@
 <head>
 <style>
 body{
-	background-image: url("formback.png");
+	background-image: url("simple.png");
 }
 
 table{
-	background-image: url("table.png");
+	background-image: url("simple.png");
 	border:2px solid black;
 	width: 45%;
 
@@ -16,13 +16,13 @@ table, th, td {
 	padding: 2px;
 	color: white;
 	font-family: monospace;
-	font-size: 24px;
-	color: #fefe70;
+	font-size: 14px;
+	
 	font-weight: bold;
-	font-style: italic;
+	font-style: unset;
 }
 </style>
-<h2 style="color: #fefe70" align="center">LIST OF APPLICANTS</h2>
+<h2 style="color: white" align="center">LIST OF APPLICANTS</h2>
 
 </head>
 <body>
@@ -40,19 +40,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT firstname, lastname,dob,gender,nationality,address,city,pincode,category,status,email,phone FROM applicants";
+$sql = "SELECT firstname, lastname,dob,gender,nationality,address,city,pincode,category,email,phone FROM applicants";
 $result = $conn->query($sql);
-
+echo"<center>";
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>First Name</th><th>Last Name</th><th>Date of Birth</th><th>Gender</th><th>Nationality</th><th>Address</th><th>City</th><th>Pincode</th><th>Category</th><th>Status</th><th>Email id</th><th>Phone Number</th></tr>";
+    echo "<table><tr><th>First Name</th><th>Last Name</th><th>Date of Birth</th><th>Gender</th><th>Nationality</th><th>Address</th><th>City</th><th>Pincode</th><th>Category</th><th>Email id</th><th>Phone Number</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["firstname"]."</td><td>".$row["lastname"]."</td><td>".$row["dob"]."</td><td>".$row["gender"]."</td><td>".$row["nationality"]."</td><td>".$row["address"]."</td><td>".$row["city"]."</td><td>".$row["pincode"]."</td><td>".$row["category"]."</td><td>".$row["status"]."</td><td>".$row["email"]."</td><td>".$row["phone"]."</td></tr>";
+        echo "<tr><td>".$row["firstname"]."</td><td>".$row["lastname"]."</td><td>".$row["dob"]."</td><td>".$row["gender"]."</td><td>".$row["nationality"]."</td><td>".$row["address"]."</td><td>".$row["city"]."</td><td>".$row["pincode"]."</td><td>".$row["category"]."</td><td>"."</td><td>".$row["email"]."</td><td>".$row["phone"]."</td></tr>";
     }
     echo "</table>";
 } else {
     echo "0 results";
 }
+echo"</center>";
 $conn->close();
 ?>
 <br /><br />

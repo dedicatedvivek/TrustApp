8561e47beca6e572e50645d386b6a4c10ea389f2
dedicatedvivek <vivekiyer98@gmail.com>
2018-07-30 22:18:38 +0530

    heas

---
+++ storage/framework/views/ab3546321972cfb8443d1da9a16bdbcb6ff4b87b.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SUCCESS</title>
+	<style type="text/css">
+		
+		#successmessage{
+
+			font-weight: bold;
+			font-family: cursive;
+			font-style: italic;
+			font-size:50%;
+			size: 80%;
+		}
+
+	</style>
+</head>
+<body>
+
+	<center>
+		<div id="successmessage">
+			RECORD SUCCESSFULLY ADDED
+		</div>
+
+		<br><br>
+
+		 <a class="nav-link" href="<?php echo e(url('/')); ?>">Back to CharApp</a>
+
+		
+	</center>
+
+</body>
+</html>
